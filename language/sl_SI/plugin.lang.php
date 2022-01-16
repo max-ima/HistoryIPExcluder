e53d4c9d0ae7f40dfb1057cb8f216e1567511c3d
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-16 23:02:44 +0100

    sl_SI
    Slovenian language

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+$lang['HIPE_description'] = 'Ta vtičnik omogoča izključitev obsegov IP ali IP iz zgodovine in statistike. <br>Njegova aktivacija blokira zapis v tabeli IP *_history, določeni v spodnji tabeli.';
+$lang['HIPE_admin_section1'] = 'Izključitev IP';
+$lang['HIPE_admin_description1'] = 'V spodnje polje vnesite celoten obseg IP ali IP, ki ga želite izključiti (ena na vrstico). Če želite določiti obseg IP, uporabite nadomestni znak "%".<br>Primer: 74.6.1.2 ali 74.6.%';
+$lang['HIPE_save_config']='Konfiguracija je shranjena.';
+$lang['HIPE_CleanHist']='Počisti zgodovino';
+
+$lang['HIPE_admin_section2'] = 'Poizvedbe v tabeli zgodovine';
+$lang['HIPE_admin_section3'] = 'Rezultat zgodovinske zahteve';
+$lang['HIPE_IPByMember'] = 'IP-ji po članu';
+$lang['HIPE_IPByMember_description'] = 'Prikaži IP-je, ki jih uporabljajo člani, razvrščene po IP-ju';
+$lang['HIPE_OnlyGuest'] = 'Samo IP-ji gostov';
+$lang['HIPE_OnlyGuest_description'] = 'Pokaži IP-je, ki jih uporabljajo samo gostje, in kolikokrat jih najdemo v tabeli zgodovine, razvrščenih po številu krat';
+
+$lang['HIPE_IPForMember'] = 'IP-ji za člana';
+$lang['HIPE_IPForMember_description'] = 'Iskanje in prikaz IP-jev, povezanih z registriranim uporabnikom (razvrščenih po IP)';
+$lang['HIPE_MemberForIp'] = 'Člani za en IP';
+$lang['HIPE_MemberForIp_description'] = 'Iskanje in prikazovanje uporabnikov, povezanih z enim IP-jem (razvrščeno po imenu)';
+
+$lang['HIPE_resquet_ok'] = 'Zahtevek v redu.';
+$lang['HIPE_hist_cleaned'] = 'Opravljeno je čiščenje tabele zgodovine.';
+
+$lang['IP_geolocalisation'] = 'Geolokalizacija';
+
+// --------- Starting below: New or revised $lang ---- from version 2.1.0
+$lang['HIPE_version'] = ' - Verzija: ';
+// --------- End: New or revised $lang ---- from version 2.1.0
+
+// --------- Starting below: New or revised $lang ---- from version 2.1.1
+$lang['HIPE_IPBlacklist_title'] = 'Črni seznam registracije';
+$lang['HIPE_IPBlacklisted'] = ' Preprečite registracijo v galerijo izključenih IP-jev (črni seznam)';
+$lang['Error_HIPE_BlacklistedIP'] = 'Napaka! Vaš IP je bil prepovedan. Ne morete se naročiti na to galerijo. Za več podrobnosti se obrnite na skrbnika.';
+// --------- End: New or revised $lang ---- from version 2.1.1
+
+$lang['submit'] = 'Pošlji';
+?>
